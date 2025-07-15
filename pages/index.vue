ac07d35be58b113b257573b151cfcae949caa8b2
Amirreza Noruzi <amirrezaa.noruzii@gmail.com>
2025-07-15 23:48:42 +0330

    Design FAQ section

--- pages/index.vue
+++ pages/index.vue
@@ -11,16 +11,10 @@
 
     <SectionSlider title="پرفروش ترین ها" :items="products"/>
 
-    <p class="text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, fugit at nihil quia illo veritatis perspiciatis consectetur! Ullam perferendis esse dolore? Quas cupiditate quod, sit dicta consectetur nemo ad corporis!
-    Dignissimos eligendi perferendis totam quisquam dolore dolores, debitis nisi incidunt repudiandae amet itaque at aspernatur impedit modi, reiciendis pariatur mollitia obcaecati? Officia ratione animi harum laudantium, eius cupiditate veritatis distinctio?
-    Autem voluptatum at magnam ex, pariatur assumenda quisquam, numquam provident ut debitis cum doloremque perspiciatis ipsum, dolorem architecto totam neque fuga facere placeat error quae voluptate quas sit! Totam, facilis!
-    Quidem voluptates, illum magni excepturi facilis eveniet saepe minus error perferendis deleniti omnis. Quibusdam doloremque, fugiat exercitationem consequatur, quae omnis esse et quas impedit earum dicta possimus, aperiam eum harum!
-    Nulla vitae quia eius provident, sequi deleniti iste! Perferendis quis optio nostrum ex nihil, asperiores, aperiam quasi placeat deserunt adipisci veniam. Laudantium, quisquam nisi. Minus quaerat ipsa placeat quae doloribus.
-    Facilis recusandae ad, dolor veniam deleniti inventore ipsum placeat? Quidem enim sequi perferendis doloremque illo voluptates voluptatum eius hic! Maxime excepturi rem blanditiis adipisci accusamus dicta facilis ex distinctio culpa!
-    Dolorem doloribus autem asperiores, ad labore repellendus reprehenderit vel quas illum error optio beatae. Aliquid numquam officia nisi, iste magnam provident quas doloribus voluptatum aliquam dignissimos est. Voluptatibus, blanditiis recusandae.
-    Praesentium at iste vel sunt error reprehenderit in modi, eaque, magni dolores explicabo exercitationem necessitatibus cupiditate officiis, magnam nisi tenetur ea reiciendis. Animi quae blanditiis assumenda debitis tenetur eum temporibus.
-    Ad minima animi enim ut eos autem illo rem, error inventore natus iure aliquid id eius itaque repellat excepturi ea iste ratione impedit magnam beatae consequuntur explicabo recusandae debitis! Suscipit.
-    Porro dolorem voluptate quas reiciendis deserunt praesentium inventore sit explicabo impedit corrupti veritatis voluptatem et minus, cumque nulla omnis quo architecto nesciunt dolorum, sapiente excepturi saepe harum fugiat iure. Beatae.</p>
+    <div class=" bg-zinc-200 my-20 p-4">
+        <h1 class="text-center text-3xl mb-7 font-bold">سوالات متداول</h1>
+        <AccordionFaq />
+    </div>
 
     <Footer />
 </template>
