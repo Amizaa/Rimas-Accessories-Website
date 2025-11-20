36a603639cf61b4a89c540b01a64acee3c1509f6
Amirreza Noruzi <amirrezanoruziiii@gmail.com>
2025-11-20 18:48:04 +0330

    update django settings

--- backend/rimasbackend/settings.py
+++ backend/rimasbackend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kn^o%*41v4l!dk%r#p5*p-!d&ra$8u+yc1&#8$#s+1x$5y#gu^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rimas-accessory.ir','www.rimas-accessory.ir','127.0.0.1','localhost']
 
 
 # Application definition
@@ -146,6 +146,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
+    "https://rimas-accessory.ir",
+    "https://www.rimas-accessory.ir",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
